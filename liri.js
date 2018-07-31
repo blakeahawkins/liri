6ebd2daf0e2a2fbc4e22361431bc9a58aa6c9304
blakeahawkins <blakeahawkins@gmail.com>
2018-07-31 16:12:42 -0500

    All but the fs log.txt do-what-it-says part is complete.

--- liri.js
+++ liri.js
@@ -4,12 +4,78 @@ var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var request = require("request");
+var fs = require("fs");
 
 var command = process.argv[2];
+var searchTerm = " ";
+for(i = 3; i < process.argv.length; i++) {
+    if(i > 3 && i < process.argv.length) {
+        searchTerm = searchTerm + " " + process.argv[i];
+    }else{
+        searchTerm += process.argv[i];
+    }
+}
+
+if(process.argv.length < 4 && command === "spotify-this-song") {
+    searchTerm = "The Sign Ace of Base";
+}
+
+if(process.argv.length < 4 && command === "movie-this") {
+    searchTerm = "Mr Nobody";
+}
+
 
 if(command === "my-tweets") {
     client.get("statuses/user_timeline", {screen_name: "blakeaaron17", count: "20"}, function(error, tweets, response) {
         if(error) throw error;
-        
+        // console.log(tweets);
+        console.log("\n   Recent Tweets:\n");
+        for(i = 0; i < tweets.length; i++) {
+            console.log(i+1 + ". " + tweets[i].text);
+            console.log("   (Tweeted: " + tweets[i].created_at.slice(0, -11) + ")\n");
+        }
+    })
+}
+if(command === "spotify-this-song") {
+    spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
+        if(err) {
+            return console.log(err);
+        }
+        var song = data.tracks.items[0];
+        // console.log(song);
+        // console.log(song.artists);
+        console.log("\nArtist(s):");
+        var artistsArr = song.artists;
+        console.log(" - " + artistsArr[0].name);
+        for(j = 1; j > artistsArr.length; j++) {
+            console.log(" - " + artistsArr[j].name);
+        }
+        console.log("\nSong Title:\n - " + song.name);
+        console.log("\nPreview Link:\n - " + song.preview_url);
+        console.log("\nAlbum:\n - " + song.album.name);
+    })
+}
+
+if(command === "movie-this") {
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&apikey=trilogy";
+    request(queryUrl, function(error, response, body) {
+        if(!error && response.statusCode === 200) {
+            var movieJSON = JSON.parse(body);
+            console.log("\nTitle:\n - " + movieJSON.Title +
+                "\n\nYear:\n - " + movieJSON.Year +
+                "\n\nCountry:\n - " + movieJSON.Country +
+                "\n\nLanguage:\n - " + movieJSON.Language +
+                "\n\nPlot:\n - " + movieJSON.Plot +
+                "\n\nActors:\n - " + movieJSON.Actors);
+            for(i = 0; i < movieJSON.Ratings.length; i++) {
+                if(movieJSON.Ratings[i].Source === "Rotten Tomatoes") {
+                    console.log("\nRotten Tomatoes Rating:\n - " + movieJSON.Ratings[i].Value);
+                }
+                if(movieJSON.Ratings[i].Source === "Internet Movie Database") {
+                    console.log("\nIMDB Rating:\n - " + movieJSON.Ratings[i].Value);
+                }
+            }
+        }
     })
 }
